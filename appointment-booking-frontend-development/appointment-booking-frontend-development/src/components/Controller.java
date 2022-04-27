1bbe62de679e1a753e25ffcbc0bd4fc325b21eaf
Steve Kim <41656946+skimchi@users.noreply.github.com>
2022-04-27 13:15:01 -0700

    completed registration java

--- appointment-booking-frontend-development/appointment-booking-frontend-development/src/components/Controller.java
+++ appointment-booking-frontend-development/appointment-booking-frontend-development/src/components/Controller.java
@@ -15,22 +15,6 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         /* Load the web page URL (location of the resource) */
-        URL url = Controller.class.getResource("/register");
         webEngine = webView.getEngine();
-        webEngine.load(url.toExternalForm());
-        /* Set the State listener as well as the name of the JavaScript object and its
-         * corresponding Java object (the class in which methods will be invoked) that
-         * will serve as the bridge for the two objects.
-         */
-        webEngine.getLoadWorker().stateProperty().addListener(new ChangeListener<State>() {
-            @Override
-            public void changed(ObservableValue<? extends State> observableValue, State oldState, State newState) {
-                if (newState == State.SUCCEEDED) {
-                    JSObject window = (JSObject) webEngine.executeScript("window");
-                    /* The two objects are named using the setMember() method. */
-                    window.setMember("invoke", new Register());
-                }
-            }
-        });
     }
 }
